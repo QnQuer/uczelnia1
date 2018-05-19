7598a5a0115897023a58989864f1bc1ac10fbe76
QnQuer <38324770+QnQuer@users.noreply.github.com>
2018-05-20 00:25:11 +0200

    1
    1

--- src/main/java/com/example/demo/model/Przedmiot.java
+++ src/main/java/com/example/demo/model/Przedmiot.java
@@ -15,8 +15,7 @@ public class Przedmiot {
     private int ects;
     @ManyToOne
     private Kierunek kierunek;
-    @ManyToMany
-    
+    @ManyToMany    
     @JoinTable(
         name = "Przedmiot_Student", 
         joinColumns = { @JoinColumn(name = "przedmiot_id") }, 
@@ -25,70 +24,7 @@ public class Przedmiot {
     private List<Student> studenci;
 
 
-    public Przedmiot() {
-    }
-
-    public Przedmiot (Long przedmiot_id, String nazwa, String opis, String rodzaj, int ects/* ,List<Book> listOfAuthorBooks*/) {
-        this.przedmiot_id = przedmiot_id;
-        this.nazwa = nazwa;
-        this.opis = opis;
-        this.rodzaj = rodzaj;
-        this.ects = ects;
-        //this.listOfAuthorBooks = listOfAuthorBooks;
-    }
-
-    public Przedmiot(String nazwa, String opis, String rodzaj, int ects) {
-        this.nazwa = nazwa;
-        this.opis = opis;
-        this.rodzaj = rodzaj;
-        this.ects = ects;
-    }
-
-    public Long getId() {
-        return przedmiot_id;
-    }
-
-    public void setId(Long przedmiot_id) {
-        this.przedmiot_id = przedmiot_id;
-    }
-
-    public String getNazwa() {
-        return nazwa;
-    }
-
-    public void setNazwa(String nazwa) {
-        this.nazwa = nazwa;
-    }
-
-    public String getOpis() {
-        return opis;
-    }
-
-    public void setOpis(String opis) {
-        this.opis = opis;
-    }
-     public String getRodzaj() {
-        return rodzaj;
-    }
-
-    public void setRodzaj(String rodzaj) {
-        this.rodzaj = rodzaj;
-    }
-     public int getEcts() {
-        return ects;
-    }
-
-    public void setEcts(int ects) {
-        this.ects = ects;
-    }
-
-
-
-  private List<Przedmiot> getListOfAuthorBooks() {
-        return listOfAuthorBooks;
-    }
-
-    public void setListOfAuthorBooks(List<Book> listOfAuthorBooks) {
-        this.listOfAuthorBooks = listOfAuthorBooks;
-    }
+   
+ 
 }
+
