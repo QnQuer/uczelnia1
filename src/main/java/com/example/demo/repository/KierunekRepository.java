06e97680daf58281a18886019ad69ab12a801c78
QnQuer <38324770+QnQuer@users.noreply.github.com>
2018-05-20 10:19:08 +0200

    1
    1

--- src/main/java/com/example/demo/repository/KierunekRepository.java
+++ src/main/java/com/example/demo/repository/KierunekRepository.java
@@ -2,18 +2,14 @@ package com.example.demo.repository;
 
 import com.example.demo.model.Kierunek;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+
 
 import java.util.List;
 
  /*
  * @author micha
  */
-@Repository
-public interface KierunekRepository {
-    public List<Kierunek> findAll();
-    public Kierunek save(Kierunek a);
-    public boolean deleteById(Kierunek s);
-    public Kierunek findOneById(Long kierunek_id);
+
+public interface KierunekRepository extends JpaRepository <Kierunek, Long> {
     
 }
--- src/main/java/com/example/demo/repository/PrzedmiotRepository.java
+++ src/main/java/com/example/demo/repository/PrzedmiotRepository.java
@@ -4,7 +4,7 @@ import com.example.demo.model.Student;
 import com.example.demo.model.Przedmiot;
 import com.example.demo.model.Wykladowca;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+
 
 import java.util.List;
 /**
@@ -12,7 +12,5 @@ import java.util.List;
  * @author micha
  */
 public interface PrzedmiotRepository extends JpaRepository <Przedmiot, Long> {
-    public Przedmiot findOneById(Long przedmiot_id);
-    public List<Przedmiot> findAllByAuthor(Student a);
-    public List<Przedmiot> findAllByAuthor(Wykladowca a);
+    
 }
